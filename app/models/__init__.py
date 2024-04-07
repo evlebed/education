57454becae275f7326d3100527e3572f9a4dbf6d
evlebed <seasea119@mail.ru>
2024-04-07 20:14:43 +0300

    Add solution for task_3

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
-from typing import Union
+from datetime import date, datetime
+from typing import List, Optional, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class ConverterRequest(BaseModel):
@@ -16,11 +17,43 @@ class User(BaseModel):
     name: str
     age: int
     adult: bool = None
+    message: Optional[str]
+
+    @validator('age')
+    def validate_age(cls, value):
+        if not 0 <= value <= 100:
+            raise ValueError("Возраст не может быть меньше 0 и больше 100")
+        return value
+
+    @property
+    def adult(self) -> bool:
+        return self.age >= 18
+
+
+class MetaMapping(BaseModel):
+    list_of_ids: List[Union[int, str]]
+    tags: set  
+
+
+class Meta(BaseModel):
+    last_modification: str
+    list_of_skills: Optional[List[str]]
+    mapping: MetaMapping
+
+    @validator('last_modification')
+    def validate_last_modification(cls, value):
+        expected_format = "%d/%m/%Y"
+        try:
+            datetime.strptime(str(value), expected_format)
+        except ValueError:
+            raise ValueError(f"Неправильный формат даты. Ожидаемый: {expected_format}")
+        return value
 
 
 class BigJson(BaseModel):
     """Использует модель User."""
     user: User
+    meta: Meta
 
 
 # class UserRequest(BaseModel):
