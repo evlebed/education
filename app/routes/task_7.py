01a231185e40559f78a54a47d8a4332b6adf8261
evlebed <seasea119@mail.ru>
2024-04-12 10:28:46 +0300

    Add solution for task_7

--- app/routes/task_7.py
+++ app/routes/task_7.py
@@ -1,8 +1,18 @@
+from time import time
 import logging
 from contextvars import ContextVar
 
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
+
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 output_log = logging.getLogger("output")
 client_host: ContextVar[str | None] = ContextVar("client_host", default=None)
@@ -22,12 +32,19 @@ client_host: ContextVar[str | None] = ContextVar("client_host", default=None)
 class CustomMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         """Load request ID from headers if present. Generate one otherwise."""
-        client_host.set(request.client.host)
-        output_log.info(f"Accepted request {request.method} {request.url}")
+        start_time = time()
+        try:
+            client_host.set(request.client.host)
+            output_log.info(f"Accepted request {request.method} {request.url}")
 
-        """Ваша реализация."""
+            response = await call_next(request)
 
-        # В случае ошибки при запросе, возвращать код 500
-        response = Response("Internal Server Error", status_code=500)
+            formatted_message = f'| {round(time() - start_time, 3)} | {request.method} | {request.url} | {response.status_code} |'
+            output_log.info(formatted_message)
+            return response
+        except:
+            formatted_message = f'| {round(time() - start_time, 3)} | {request.method} | {request.url} | 500 |'
+            output_log.error(formatted_message)
+            response = Response("Internal Server Error", status_code=500)
 
-        return response
+            return response
